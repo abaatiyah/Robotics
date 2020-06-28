440adca31b9b2a2a2bcf5d24159772ea7f538a46
abaatiyah <58458484+abaatiyah@users.noreply.github.com>
2020-06-28 04:42:39 +0300

    Add files via upload

---
+++ Motors-Without-Relay/Motor-Servo/Motor-Servo1/Servo1.ino
@@ -0,0 +1,23 @@
+#include<Servo.h>
+Servo servo;
+
+
+
+void setup() {
+ servo.attach(10);
+
+}
+
+void loop() {
+  
+  servo.write(0);
+  delay(2000);
+
+  servo.write(90);
+  delay(2000);
+
+  servo.write(180);
+  delay(2000);
+  
+
+}
